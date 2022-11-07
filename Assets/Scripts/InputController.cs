6947e2b5024f76eaad254ec4cf5add0f4846e2f1
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-11-07 11:21:10 +0100

    Script Update : InputController : Quaternions

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class InputController : Controller
 {
+    public float XSensivity = 360; //Degres par seconde par valeur d input.
+    public float YSensivity = 360; //Degres par seconde par valeur d input.
     public string MouseXName = "Mouse X";
     public string MouseYName = "Mouse Y";
 
@@ -18,8 +20,13 @@ public class InputController : Controller
         //Deadzone deja gérée par les axis
         if (axisLook.x != 0 || axisLook.y != 0)
         {
-
+            Vector3 WantedDirectionLookRight = Vector3.Cross(Vector3.up, WantedDirectionLook);
+            Quaternion rotateHorizontal = Quaternion.AngleAxis(axisLook.x * XSensivity * Time.deltaTime, Vector3.up);
+            Quaternion rotateVertical = Quaternion.AngleAxis(-axisLook.y * YSensivity * Time.deltaTime, WantedDirectionLookRight);
+            WantedDirectionLook = rotateHorizontal * rotateVertical * WantedDirectionLook;
         }
 
+        //On affiche le debug
+        DrawDebug();
     }
 }
