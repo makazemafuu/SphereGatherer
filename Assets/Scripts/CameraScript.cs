c63ea8c8c6d7f4647280463e31fd06931fbe4c0e
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-11-07 15:17:54 +0100

    Fixed Script (all into one)

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    public float mouseSpeed = 4;
+    public Transform Player;
+    public float distanceToPlayer = 4;
 
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
+    public float upDownMax = 80;
+    public float upDownMin = -80;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    //Pour lisser le mouvement
+    public float rotationSmoothTime = 0.12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
 
-    // Start is called before the first frame update
-    void Start()
+    private float orbit;
+    private float upDown;
+    void LateUpdate()
     {
+        //On récupère les mouvements de la souris
+        upDown -= Input.GetAxis("Mouse Y") * mouseSpeed;
+        orbit += Input.GetAxis("Mouse X") * mouseSpeed;
 
-    }
+        //On limite les mouvements verticaux afin de ne pas passer en dessous du joueur
+        upDown = Mathf.Clamp(upDown, upDownMin, upDownMax);
 
-    // Update is called once per frame
-    void Update()
-    {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        //On effectue la rotation de la caméra
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(upDown, orbit), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        //On place la caméra à une certaine distance du joueur
+        transform.position = Player.position - transform.forward * distanceToPlayer;
     }
 }
