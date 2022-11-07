f3aa88adf5fc212859cd5e34f7e454d276c86381
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-11-07 11:05:41 +0100

    Sound Radius Fixed

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class InputController : Controller
 {
+    public string MouseXName = "Mouse X";
+    public string MouseYName = "Mouse Y";
 
     // Update is called once per frame
     void Update()
     {
 
+        //Lecture des inputs
+        Vector2 axisLook = new Vector2(Input.GetAxis(MouseXName), Input.GetAxis(MouseYName));
+
+        //Rotation de la direction de look si input sur les axes
+        //Deadzone deja gérée par les axis
+        if (axisLook.x != 0 || axisLook.y != 0)
+        {
+
+        }
 
     }
 }
