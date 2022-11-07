d5b1ac59cc5c0b21e6b30fa24230f34d79b404de
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-11-07 16:36:34 +0100

    Gun Script Update

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public PlayerMove MyController;
+    public Controller MyController;
     public Transform PrefabProjectile;
     public float ProjectileStartSpeed = 50;
     public float OffsetForwardShoot = 2;
+    public float TimeBetweenShots = 0.5f;
+    private float TimeShoot = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if (MyController.WantsToShoot)
+        TimeShoot -= Time.deltaTime;
+
+        if (MyController.WantsToShoot && TimeShoot <= 0)
         {
+            TimeShoot = TimeBetweenShots;
+
             //Création du projetctile au bon endroit
-            Transform proj = GameObject.Instantiate<Transform>(PrefabProjectile,
+            Transform proj = GameObject.Instantiate<Tranform>(PrefabProjectile,
                 transform.position + transform.forward * OffsetForwardShoot, transform.rotation);
             //Ajout d une impulsion de départ
             proj.GetComponent<Rigidbody>().AddForce(transform.forward * ProjectileStartSpeed, ForceMode.Impulse);
