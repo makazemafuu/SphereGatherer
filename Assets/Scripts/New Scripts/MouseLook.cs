dbfc8d8bbc3cde3b4b334e60d77674d27e8f262e
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-11-08 14:34:35 +0100

    Unity Script Added

--- Assets/Scripts/New Scripts/MouseLook.cs
+++ Assets/Scripts/New Scripts/MouseLook.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLook : MonoBehaviour
+public class MouseLook : MonoBehaviour
 {
 
     public float mouseSensitivity = 100f;
