ff34de9b1422cb471fd52a1f22216c7a0bb8e4d7
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-11-08 16:01:14 +0100

    Fixed Player Movement Script (jump, slopes, stairs, isGrounded, etc)

--- Assets/Scripts/New Scripts/PLayerMovement.cs
+++ Assets/Scripts/New Scripts/PLayerMovement.cs
@@ -7,12 +7,25 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; //not 0f because this might happen before we are on the ground, so to make sure the player IS indeed on the ground we put a slightly lower negative number
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -23,6 +36,12 @@ public class PlayerMovement : MonoBehaviour
 
         //function Move, framerate independent & with a given speed
         controller.Move(move * speed * Time.deltaTime);
+
+        //to allow the player to jump
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
