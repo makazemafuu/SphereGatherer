1c46df01338b20daaa2eccbcc36bc8e5bace9f8f
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-11-09 10:55:11 +0100

    Scripts (updates) + new script (lift mouse control)

---
+++ Assets/Scripts/Prof'/LiftMouseControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftMouseControl : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("Up");
+            animator.ResetTrigger("Down");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            animator.ResetTrigger("Up");
+            animator.SetTrigger("Down");
+        }
+    }
+}
